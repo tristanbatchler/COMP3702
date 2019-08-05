df722862cef01fae35deb0ea1270e670e57ed53a
Tristan Batchler <batchlertristan@gmail.com>
2019-08-05 21:01:47 +1000

    testing pushnote

---
+++ pushnote.sh
@@ -0,0 +1,28 @@
+MODIFIED="$(git status | grep modified | sed 's/\(.*modified:\s*\)//')"
+ADDED="$(git status | grep "new file" | sed 's/\(.*new file:\s*\)//')"
+
+git add *
+git commit -m "$1"
+git push origin development
+
+git checkout master
+git checkout development .
+
+while read -r line; do
+    if [[ "$line" == *.md ]] 
+    then
+      python githubify.py "$line"  
+      git add "$line"
+    fi
+done <<< "$MODIFIED"
+
+while read -r line; do
+    if [[ "$line" == *.md ]] 
+    then
+      python githubify.py "$line"  
+      git add "$line"
+    fi
+done <<< "$ADDED"
+
+git commit -m "$1"
+git push origin master
